3d345f01c32939ceaaed539a46fb7f815eac4893
Rolf Buchwald Alvarez <rolf_buchwald@gmx.net>
2020-10-19 20:30:07 +0200

    Create the action class HelloWorldAction.java

---
+++ src/main/java/org/apache/struts/helloworld/action/HelloWorldAction.java
@@ -0,0 +1,33 @@
+package org.apache.struts.helloworld.action;
+
+// Imports the MessageStore class (Model)
+import org.apache.struts.helloworld.model.MessageStore;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+
+/* An Action class is needed to act as the Controller. It responds to a user action (in this example
+that action will be clicking an HTML hyperlink and sending a specific URL to the Servlet container).
+One or more of the Action class's methods are executed and a String result is returned. Based on the
+value of the result, a specific view page (here: HelloWorld.jsp) is rendered. */
+
+public class HelloWorldAction extends ActionSupport{
+
+    // Dependency Injection ?!?
+    private MessageStore messageStore;
+
+    public String execute() {
+        messageStore = new MessageStore();
+        return SUCCESS;
+    }
+
+    public MessageStore getMessageStore() {
+        return messageStore;
+    }
+}
+
+/* The Struts 2 framework will create an object of the HelloWorldAction class and call the execute
+method in response to a user’s action (clicking on a hyperlink that sends a specific URL to the Servlet
+container). In this example, the execute method creates an object of class MessageStore and then returns
+the String constant SUCCESS. In order to make the MessageStore object available to the view page, HelloWorld.jsp,
+the JavaBean-style of providing getter and setter methods is needed. */
