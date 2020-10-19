de11c2d29d71d5c8c88c555648b456c50106a52f
Rolf Buchwald Alvarez <rolf_buchwald@gmx.net>
2020-10-19 20:15:40 +0200

    Create the 'Model' class MessageStore.java

---
+++ src/main/java/org/apache/struts/helloworld/model/MessageStore.java
@@ -0,0 +1,18 @@
+package org.apache.struts.helloworld.model;
+
+// A model class to store the message
+/* The Struts 2 framework requires that objects exposed to the view (HelloWorld.jsp)
+* follow the JavaBean-style conventions.
+*/
+
+public class MessageStore {
+    private final String message;
+
+    public MessageStore() {
+        message = "Hello Struts User";
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
