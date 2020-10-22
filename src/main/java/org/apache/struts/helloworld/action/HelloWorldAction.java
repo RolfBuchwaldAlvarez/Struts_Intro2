ae5702b1483d09b89489a182dcb85a3248d026f0
Rolf Buchwald Alvarez <rolf_buchwald@gmx.net>
2020-10-22 12:36:34 +0200

    Add comment to HelloWorldAction.java

--- src/main/java/org/apache/struts/helloworld/action/HelloWorldAction.java
+++ src/main/java/org/apache/struts/helloworld/action/HelloWorldAction.java
@@ -5,14 +5,17 @@ package org.apache.struts.helloworld.action;
 // Imports the MessageStore class (MVC MODEL)
 import org.apache.struts.helloworld.model.MessageStore;
 
-// S2 Action classes usually extend the ActionSupport class!
+/* S2 Action classes usually extend the ActionSupport class! Class ActionSupport provides default
+   implementations for the most common actions (e.g. execute, input) and also implements several useful
+   S2 interfaces. If Action class extends class ActionSupport, it can either override or inherit the
+   default implementations. */
 import com.opensymphony.xwork2.ActionSupport;
 
 
 /* An Action class is needed to act as the Controller. It responds to a user action (in this example
-that action will be clicking an HTML hyperlink and sending a specific URL to the Servlet container).
-One or more of the Action class's methods are executed and a String result is returned. Based on the
-value of the result, a specific view page (here: HelloWorld.jsp) is rendered. */
+   that action will be clicking an HTML hyperlink and sending a specific URL to the Servlet container).
+   One or more of the Action class's methods are executed and a String result is returned. Based on the
+   value of the result, a specific view page (here: HelloWorld.jsp) is rendered. */
 
 public class HelloWorldAction extends ActionSupport{
 
@@ -39,7 +42,7 @@ public class HelloWorldAction extends ActionSupport{
 }
 
 /* The Struts 2 framework will create an object of the HelloWorldAction class and call the execute
-method in response to a user’s action (clicking on a hyperlink that sends a specific URL to the Servlet
-container). In this example, the execute method creates an object of class MessageStore and then returns
-the String constant SUCCESS. In order to make the MessageStore object available to the view page, HelloWorld.jsp,
-the JavaBean-style of providing getter and setter methods is needed. */
+   method in response to a user’s action (clicking on a hyperlink that sends a specific URL to the Servlet
+   container). In this example, the execute method creates an object of class MessageStore and then returns
+   the String constant SUCCESS. In order to make the MessageStore object available to the view page, HelloWorld.jsp,
+   the JavaBean-style of providing getter and setter methods is needed. */
