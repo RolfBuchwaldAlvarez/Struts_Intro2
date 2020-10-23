647a85804649ad8e4af148729fd6d6b926d3b348
Rolf Buchwald Alvarez <rolf_buchwald@gmx.net>
2020-10-23 10:11:15 +0200

    Create RegisterAction class

---
+++ src/main/java/org/apache/struts/register/action/RegisterAction.java
@@ -0,0 +1,29 @@
+package org.apache.struts.register.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts.register.model.Person;
+
+public class RegisterAction extends ActionSupport {
+    /* By using a Java model class to encapsulate the data provided by the form, separate attributes (with
+       get/set methods) for each form field are unnecessary. */
+    private static final long serialVersionUID = 1L;
+    private Person personBean;
+
+    // Specified in struts.xml to be called in response to the register action
+    /* In a real application, within execute() we would call upon other classes (Service objects) to
+       perform business processing of the form, such as storing user's input into a data repository. */
+    @Override
+    public String execute() throws Exception {
+        //call Service class to store personBean's state in database
+
+        return SUCCESS;
+    }
+
+    public Person getPersonBean() {
+        return personBean;
+    }
+
+    public void setPersonBean(Person person) {
+        this.personBean = person;
+    }
+}
